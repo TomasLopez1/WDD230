81c902cf749d4a5498a7af32443c5081aecbab92
TomasLopez1 <tomasjlopezz@gmail.com>
2022-09-23 19:17:08 -0300

    update

---
+++ js/actualDate.js
@@ -0,0 +1,4 @@
+const d = new Date();
+
+document.getElementById("footer").textContent = `© ${d.getFullYear()} | Tomas Lopez | BYU Idaho`;
+document.getElementById("date").textContent = `Last Updated: ${document.lastModified}`;
