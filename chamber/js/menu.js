58c0c1a0c30e18258dcc2d185e2aa709c8f8eea2
TomasLopez1 <tomasjlopezz@gmail.com>
2022-10-14 09:13:54 -0300

    chamber update

--- chamber/js/menu.js
+++ chamber/js/menu.js
@@ -6,3 +6,13 @@ function toggleMenu() {
   
   const x = document.getElementById('hamburgerBtn')
   x.onclick = toggleMenu;
+
+  //--- Banner ---//
+const dayNumber = now.getDay();
+const element = document.getElementById("banner");
+
+if (dayNumber == 1 || dayNumber == 2 ) {
+    element.classList.add("showme");
+} else {
+    element.classList.add("hideme");
+}
